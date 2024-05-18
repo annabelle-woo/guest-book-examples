1dbfb0020fc34bcee112066a124de56242e3eda7
annebelle-woo <wooa8@mcmaster.com>
2024-05-18 17:38:56 -0400

    Init

--- contract-ts/src/contract.ts
+++ contract-ts/src/contract.ts
@@ -1,24 +1,64 @@
 import { NearBindgen, near, call, view, Vector } from 'near-sdk-js'
-import { POINT_ONE, PostedMessage } from './model'
+import { POINT_ONE, Claim } from './model'
 
 @NearBindgen({})
 class GuestBook {
-  messages: Vector<PostedMessage> = new Vector<PostedMessage>("v-uid");
+  messages: Vector<Claim> = new Vector<Claim>("v-uid");
 
   @call({ payableFunction: true })
   // Public - Adds a new message.
-  add_message({ text }: { text: string }) {
-    // If the user attaches more than 0.1N the message is premium
+  claim({ 
+    policy_deductable,
+    incident_date,
+    policy_annual_premium, 
+    incident_type, 
+    collision_type,
+    incident_severity,
+    authorities_contacted,
+    number_of_vehicles_involved,
+    police_report_available,
+    total_claim_amount,
+    injury_claim,
+    property_claim,
+    vehicle_claim,
+    }: { 
+    policy_deductable: number, 
+    incident_date: string, policy_annual_premium: number, incident_type: string, collision_type: string,
+    incident_severity: string,
+    authorities_contacted: string,
+    number_of_vehicles_involved: number,
+    police_report_available: string,
+    total_claim_amount: number,
+    injury_claim: number,
+    property_claim: number,
+    vehicle_claim: number, }) {
+    
     const premium = near.attachedDeposit() >= BigInt(POINT_ONE);
     const sender = near.predecessorAccountId();
-
-    const message: PostedMessage = { premium, sender, text };
+    let account_Id = near.predecessorAccountId();
+    
+    const message: Claim = {
+    account_Id,
+    policy_deductable, 
+    incident_date, 
+    policy_annual_premium, 
+    incident_type, 
+    collision_type, 
+    incident_severity, 
+    authorities_contacted,
+    number_of_vehicles_involved,
+    police_report_available,
+    total_claim_amount,
+    injury_claim, 
+    property_claim, 
+    vehicle_claim
+    } ;
     this.messages.push(message);
   }
 
   @view({})
   // Returns an array of messages.
-  get_messages({ from_index = 0, limit = 10 }: { from_index: number, limit: number }): PostedMessage[] {
+  get_messages({ from_index = 0, limit = 10 }: { from_index: number, limit: number }): Claim[] {
     return this.messages.toArray().slice(from_index, from_index + limit);
   }
 
--- contract-ts/src/model.ts
+++ contract-ts/src/model.ts
@@ -1,13 +1,52 @@
 export const POINT_ONE = '100000000000000000000000';
 
-export class PostedMessage {
-  premium: boolean;
-  sender: string;
-  text: string;
+export class Claim {
+  account_Id: string;
+  policy_deductable: number;               // Corresponds to field 6
+  incident_date: string;               // Corresponds to field 17 but should likely be stored as a date type or timestamp
+  policy_annual_premium: number;           // float64
+  incident_type: string;                   // object
+  collision_type: string;                  // object
+  incident_severity: string;              // object
+  authorities_contacted: string;           // object
+  number_of_vehicles_involved: number;     // int64
+  police_report_available: string;         // object
+  total_claim_amount: number;              // int64
+  injury_claim: number;                    // int64
+  property_claim: number;                  // int64
+  vehicle_claim: number;                   // int64
 
-  constructor({ premium, sender, text }: PostedMessage) {
-    this.premium = premium;
-    this.sender = sender;
-    this.text = text;
+
+  constructor({
+    account_Id,
+    policy_deductable,
+    incident_date, 
+    policy_annual_premium, 
+    incident_type, 
+    collision_type,
+    incident_severity,
+    authorities_contacted,
+    number_of_vehicles_involved,
+    police_report_available,
+    total_claim_amount,
+    injury_claim,
+    property_claim,
+    vehicle_claim, }: Claim) {
+    this.account_Id = account_Id;
+    this.policy_deductable = policy_deductable;
+    this.incident_date = incident_date;
+    this.policy_annual_premium = policy_annual_premium;
+    this.incident_type = incident_type;
+    this.collision_type = collision_type
+    this.incident_severity = incident_severity;
+    this.incident_date = incident_date;
+    this.authorities_contacted = authorities_contacted;
+    this.number_of_vehicles_involved = number_of_vehicles_involved;
+    this.police_report_available = police_report_available;
+    this.incident_date = incident_date;
+    this.total_claim_amount = total_claim_amount;
+    this.injury_claim = injury_claim;
+    this.property_claim = property_claim;
+    this.vehicle_claim = vehicle_claim;
   }
 }
